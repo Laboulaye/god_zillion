f7ae5c766138745c711b28ebdca54fdaad788ea3
mdemianov <maks.demianov@gmail.com>
2023-12-04 20:12:33 +0200

    Change signin to POST request

---
+++ src/main/java/student/examples/ggengine/domain/dto/UserAuthDto.java
@@ -0,0 +1,15 @@
+package student.examples.ggengine.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAuthDto {
+	
+	private String userName;
+	private String password;
+
+}
